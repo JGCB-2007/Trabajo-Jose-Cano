105dd63d4c692ef1f5cb353896edd8c37d5fb82a
JGCB-2007 <jgcano@uamv.edu.ni>
2025-05-13 21:38:50 -0600

    Delete Trabajo clase 12.05.25 directory

--- Trabajo clase 12.05.25/Ejercicio 1/menu.py
+++
@@ -1,8 +0,0 @@
-# menu.py
-
-def mostrar_menu():
-    print("\n--- Menú de Atención Clínica ---")
-    print("1. Registrar paciente")
-    print("2. Atender paciente")
-    print("3. Ver pacientes en espera")
-    print("4. Salir")
--- Trabajo clase 12.05.25/Ejercicio 1/nodo.py
+++
@@ -1,6 +0,0 @@
-# nodo.py
-
-class Nodo:
-    def __init__(self, dato):
-        self.dato = dato
-        self.siguiente = None
